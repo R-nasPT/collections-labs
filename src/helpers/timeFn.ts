be37966f4c1c50938cf192bd4624e24893b0818a
Yotsatorn  Pathan <120444604+R-nasPT@users.noreply.github.com>
2024-03-26 18:53:58 +0700

    add nid noi

--- src/helpers/timeFn.ts
+++ src/helpers/timeFn.ts
@@ -1,6 +1,16 @@
 // เมื่อหลังจาก 1 สัปดาห์ ลบค่า 'seller-portal-onboarding' ออกจาก localStorage
+const [open, setOpen] = useState(true)
+
+useEffect(() => {
+  const oneWeek = localStorage.getItem('seller-centre')
+  if (oneWeek) {
+    setOpen(false)
+  }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+}, [])
+
 setTimeout(function() {
-    localStorage.removeItem('seller-portal-onboarding');
+    localStorage.removeItem('test-mock-onboarding');
 }, 7 * 24 * 60 * 60 * 1000); // 1 สัปดาห์มี 7 วัน, 1 วันมี 24 ชั่วโมง, 1 ชั่วโมงมี 60 นาที, 1 นาทีมี 60 วินาที, 1 วินาทีมี 1000 มิลลิวินาที
 
 
@@ -8,7 +18,7 @@ setTimeout(function() {
 useEffect(() => {
     const timeout = setTimeout(
       () => {
-        localStorage.removeItem('seller-centre')
+        localStorage.removeItem('test-mock-onboarding')
       },
       1000,
     )
@@ -16,3 +26,9 @@ useEffect(() => {
     // เมื่อ Component ถูก Unmount ให้ลบ timeout เพื่อป้องกันการเกิด memory leak
     return () => clearTimeout(timeout)
   }, []) // ให้ useEffect ทำงานเมื่อ Component ถูกโหลดครั้งแรกเท่านั้น
+
+  
+  const onClose = () => {
+    localStorage.setItem('seller-centre', 'true')
+    setOpen(false)
+  }
