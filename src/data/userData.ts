409ce0b348f1f792865759f965c56740517d35fc
Yotsatorn  Pathan <120444604+R-nasPT@users.noreply.github.com>
2024-06-06 17:18:26 +0700

    Create userData.ts

---
+++ src/data/userData.ts
@@ -0,0 +1,15 @@
+export const userData = [
+  {
+    id: 1,
+    name: 'John Doe',
+    email: 'john.doe@example.com',
+    // other properties
+  },
+  {
+    id: 2,
+    name: 'Jane Smith',
+    email: 'jane.smith@example.com',
+    // other properties
+  },
+  // more items
+];
