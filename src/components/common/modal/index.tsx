a21de0a891a3df15f000f25500bbca4b52377226
Yotsatorn  Pathan <120444604+R-nasPT@users.noreply.github.com>
2024-06-14 14:23:55 +0700

    Update index.tsx

--- src/components/common/modal/index.tsx
+++ src/components/common/modal/index.tsx
@@ -23,7 +23,7 @@ export default function Modal({ open, onClose, children }: ModalProps) {
         {/* modal */}
         <div
           className={`bg-white rounded-xl shadow p-6 transition-all ${
-            open ? "scale-100 opacity-100" : "scale-125 opacity-0"
+            open ? "scale-100 opacity-100" : "scale-125 opacity-0"        //<--- scale สามารถสลับกันได้
           }`}
           onClick={(e) => e.stopPropagation()}
         >
