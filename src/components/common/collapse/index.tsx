af04725835a9653a4ff0de3a7f016e242d9c6b49
Yotsatorn  Pathan <120444604+R-nasPT@users.noreply.github.com>
2024-06-15 23:27:17 +0700

    Update index.tsx

--- src/components/common/collapse/index.tsx
+++ src/components/common/collapse/index.tsx
@@ -21,12 +21,22 @@ export default function Collapse({ title, children }: CollapseProps) {
         <div className={`w-full ${open ? 'font-bold': ''}`}>
           {title}
         </div>
-        <div
-        //className="text-xl rotate-90 cursor-pointer duration-500 group-[.is-active]:rotate-[270deg]"   //<-- หรือจะใช็ แบบนี้ก็ได้ถ้าใช้การตั่งชื่อ class แล้วเอามา group
-          className={`text-xl duration-500 ${open ? 'rotate-90': 'rotate-[270deg]'}`}
-        >
-          {">"}
-        </div>
+        <span 
+        //className="shrink-0 transition duration-500 group-[.is-active]:-rotate-180"   //<-- หรือจะใช้ แบบนี้ก็ได้ถ้าใช้การตั่งชื่อ class แล้วเอามา group
+          className={`shrink-0 transition duration-500 ${open ? '-rotate-180': ''}`}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </span>
       </div>
 
       {/* Collapse */}
