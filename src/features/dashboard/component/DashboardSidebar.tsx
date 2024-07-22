44b2e8ff68525d3bb38691dabc7d36a8cbba52e9
Yotsatorn  Pathan <120444604+R-nasPT@users.noreply.github.com>
2024-07-22 13:30:47 +0700

    Create DashboardSidebar.tsx

---
+++ src/features/dashboard/component/DashboardSidebar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './DashboardHeader.module.css';
+
+const DashboardHeader = () => {
+    return (
+        <aside className="dashboard-header">
+            <h1>Dashboard Sidebar</h1>
+        </aside>
+    );
+}
+
+export default DashboardHeader;
