552b1c88448053678ee042ef2120aa54d2909e21
Yotsatorn  Pathan <120444604+R-nasPT@users.noreply.github.com>
2024-07-22 13:39:14 +0700

    Create IconLibrary.ts

---
+++ src/libs/IconLibrary.ts
@@ -0,0 +1,10 @@
+import { FaHome, FaUser, FaCog } from 'react-icons/fa';
+import { MdSearch, MdFavorite } from 'react-icons/md';
+
+export {
+  FaHome,
+  FaUser,
+  FaCog,
+  MdSearch,
+  MdFavorite
+};
