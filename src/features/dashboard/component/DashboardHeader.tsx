f68734cfb17a44bfa0612d44bf9393f11a4c5fd0
Yotsatorn  Pathan <120444604+R-nasPT@users.noreply.github.com>
2024-07-22 13:29:05 +0700

    Create DashboardHeader.tsx

---
+++ src/features/dashboard/component/DashboardHeader.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './DashboardHeader.module.css';
+
+const DashboardHeader = () => {
+    return (
+        <header className="dashboard-header">
+            <h1>Dashboard</h1>
+        </header>
+    );
+}
+
+export default DashboardHeader;
