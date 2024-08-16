4391fee62b1ffddb15e4d1b3e775a21563139af5
Yotsatorn  Pathan <120444604+R-nasPT@users.noreply.github.com>
2024-08-16 15:18:28 +0700

    Update toast.tsx

--- src/components/ui/toast/toast.tsx
+++ src/components/ui/toast/toast.tsx
@@ -1,36 +1,41 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 interface ToastProps {
   content: React.ReactNode | ((props: { onClose: () => void }) => React.ReactNode);
   duration?: number;
   onClose: () => void;
+  onHeightChange: (height: number) => void;
 }
 
-export default function Toast({ content, duration, onClose }: ToastProps) {
+export default function Toast({ content, duration, onClose, onHeightChange  }: ToastProps) {
     const [isVisible, setIsVisible] = useState(false);
-    const [isClosing, setIsClosing] = useState(false);
-    
-    const closeToast = () => {
-      setIsClosing(true);
-      setIsVisible(false);
-      setTimeout(onClose, 500); // รอให้ animation เสร็จสิ้นก่อนเรียก onClose
-    };
-    
+    const toastRef = useRef<HTMLDivElement>(null);
+  
     useEffect(() => {
-      setIsVisible(true);
+      setTimeout(() => setIsVisible(true), 10);
+      
       if (duration !== undefined) {
-        const timer = setTimeout(() => {
-          closeToast();
-        }, duration);
+        const timer = setTimeout(closeToast, duration);
         return () => clearTimeout(timer);
       }
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [duration]);
   
+    useEffect(() => {
+      if (toastRef.current) {
+        onHeightChange(toastRef.current.offsetHeight);
+      }
+    }, [onHeightChange]);
+  
+    const closeToast = () => {
+      setIsVisible(false);
+      setTimeout(onClose, 500);
+    };
+  
 
   const renderContent = () => {
     if (typeof content === 'function') {
-      return content({ onClose });
+      return content({ onClose: closeToast });
     }
     return content;
   };
@@ -40,8 +45,7 @@ export default function Toast({ content, duration, onClose }: ToastProps) {
       className={`
         transform transition-all duration-500 ease-out
         ${isVisible ? 'translate-y-0 opacity-100' : '-translate-y-full opacity-0'}
-        ${isClosing ? 'pointer-events-none' : ''}
-      `}
+        `}
     >
       {renderContent()}
     </div>
