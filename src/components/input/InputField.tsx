5c0d126d8c39d67c070390c71cdcf9be6ceafd02
Yotsatorn  Pathan <120444604+R-nasPT@users.noreply.github.com>
2024-08-23 12:32:14 +0700

    Update InputField.tsx

--- src/components/input/InputField.tsx
+++ src/components/input/InputField.tsx
@@ -4,12 +4,6 @@ import { FieldErrors, UseFormRegister } from "react-hook-form";
 
 interface FormValues {
   [key: string]: any;
-}import { useNoScroll } from "@/hooks";
-import { cn } from "@/utils/cn";
-import { FieldErrors, UseFormRegister } from "react-hook-form";
-
-interface FormValues {
-  [key: string]: any;
 }
 
 interface InputFieldProps {
@@ -59,50 +53,6 @@ export default function InputField({
           "pointer-events-none peer-focus:font-medium",
           errors?.[name] ? "text-[#ff3000]" : "text-gray-800 bg-white"
         )}
-      >
-        {placeholder}
-      </label>
-
-      <p className="text-[#ff3506] text-xs h-1 pl-2">
-        {errors?.[name] && `${placeholder.toLowerCase()} is a required field`}
-      </p>
-    </div>
-  );
-}
-
-
-interface InputFieldProps {
-  name: string;
-  placeholder: string;
-  register: UseFormRegister<FormValues>;
-  errors: FieldErrors<FormValues>;
-  type?: string;
-  className?: string;
-}
-
-export default function InputField({ name, placeholder, register, type = "text", errors, className }: InputFieldProps) {
-  const handleNoScroll = useNoScroll();
-
-  return (
-    <div className={cn("relative", className)}>
-      <input
-        id={name}
-        className={`peer py-3 pl-5 rounded-2xl w-full text-base border rm-arrow-spin ${
-          errors?.[name]
-            ? "border-[#ff3506] focus:outline-[#ff3506]"
-            : "border-[#24075c] hover:border-[#531ae3] focus:outline-[#531ae3] text-[#280d5f]"
-        }`}
-        type={type}
-        onWheel={handleNoScroll}
-        required
-        {...register(name)}
-      />
-
-      <label
-        htmlFor={name}
-        className={`absolute top-[23%] left-5 px-1 text-[#433958] transition-all duration-300 peer-focus:-top-2 peer-focus:text-xs peer-valid:-top-2 peer-valid:text-xs pointer-events-none peer-focus:font-medium ${
-          errors?.[name] ? "text-[#ff3000]" : "text-gray-800 bg-white"
-        }`}
       >
         {placeholder}
       </label>
