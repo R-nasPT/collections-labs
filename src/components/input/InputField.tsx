25adb8c5b0da07b7c9b8ca3f0d0be1459bb8f335
Yotsatorn  Pathan <120444604+R-nasPT@users.noreply.github.com>
2024-09-16 10:14:27 +0700

    Update InputField.tsx

--- src/components/input/InputField.tsx
+++ src/components/input/InputField.tsx
@@ -1,65 +1,72 @@
+import { useState, useEffect } from "react";
+import { cn } from "@/utils";
 import { useNoScroll } from "@/hooks";
-import { cn } from "@/utils/cn";
-import { FieldErrors, UseFormRegister } from "react-hook-form";
-
-interface FormValues {
-  [key: string]: any;
-}
 
 interface InputFieldProps {
   name: string;
   placeholder: string;
-  register: UseFormRegister<FormValues>;
-  errors: FieldErrors<FormValues>;
+  value: string;
+  onChange: (value: string) => void;
   type?: string;
-  required?: boolean;
+  error?: string;
+  wrapperStyles?: string;
   className?: string;
+  labelClassName?: string;
 }
 
 export default function InputField({
   name,
   placeholder,
-  register,
+  value,
+  onChange,
   type = "text",
-  errors,
-  required = true,
+  error,
+  wrapperStyles,
   className,
+  labelClassName,
 }: InputFieldProps) {
+  const [isFilled, setIsFilled] = useState(false);
   const handleNoScroll = useNoScroll();
 
+  useEffect(() => {
+    setIsFilled(!!value);
+  }, [value]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onChange(e.target.value);
+  };
+
   return (
-    <div className="relative">
+    <div className={cn("relative", wrapperStyles)}>
       <input
         id={name}
         className={cn(
           "peer py-3 pl-5 rounded-2xl w-full text-base border rm-arrow-spin",
-          errors?.[name]
+          error
             ? "border-[#ff3506] focus:outline-[#ff3506]"
             : "border-[#24075c] hover:border-[#531ae3] focus:outline-[#531ae3] text-[#280d5f]",
           className
         )}
         type={type}
-        onWheel={handleNoScroll}
-        required={required}
-        {...register(name)}
+        value={value}
+        onWheel={type === "number" ? handleNoScroll : undefined}
+        onChange={handleChange}
       />
 
       <label
         htmlFor={name}
         className={cn(
-          "absolute top-[23%] left-5 px-1 rounded-full text-[#433958] transition-all duration-300",
-          "peer-focus:-top-2 peer-focus:text-[#531ae3] peer-focus:text-xs",
-          "peer-valid:-top-2 peer-valid:text-xs",
+          "absolute top-[18%] left-5 px-1 bg-white rounded-full transition-all duration-300 italic",
+          "peer-focus:-top-2 peer-focus:text-xs",
+          labelClassName,
+          isFilled && "-top-2 text-xs",
           "pointer-events-none peer-focus:font-medium",
-          errors?.[name] ? "text-[#ff3000]" : "text-gray-800 bg-white"
+          error ? "text-[#fa8383] peer-focus:text-[#ff0606]" : "text-gray-400 peer-focus:text-[#531ae3]"
         )}
       >
         {placeholder}
       </label>
-
-      <p className="text-[#ff3506] text-xs h-1 pl-2">
-        {errors?.[name] && `${placeholder.toLowerCase()} is a required field`}
-      </p>
+      {error && <p className="text-[#ff3506] text-xs pl-3">{error}</p>}
     </div>
   );
 }
