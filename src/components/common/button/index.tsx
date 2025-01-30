7e59efdfff614979df438efd32d54ee3db550a08
Yotsatorn  Pathan <120444604+R-nasPT@users.noreply.github.com>
2025-01-30 17:42:40 +0700

    Delete src/components/common/button directory

--- src/components/common/button/index.tsx
+++
@@ -1,7 +0,0 @@
-export default function Button({ children }: { children: React.ReactNode }) {
-  return (
-    <button className="flex gap-2 px-4 py-2 rounded-lg bg-blue-600 text-white font-medium">
-      {children}
-    </button>
-  );
-}
